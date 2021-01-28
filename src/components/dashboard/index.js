f4b81cda65f94a46e23ca84cd8b9663f5af3db97
Pratik <pratikroychowdhury91011@gmail.com>
2021-01-28 10:43:56 +0530

    updated the nickname feature in the dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Button, Drawer } from 'rsuite'
-import { useProfile } from '../../context/profile.context'
+import React from 'react';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
 
-  const {profile}=useProfile()
+  const {profile}=useProfile();
+  
+  const onSave=async(newData)=>{
+
+    const userNicknameRef =  database.ref(`/profiles/${profile.uid}`).child('name');
+    
+    try{
+      await userNicknameRef.set(newData);
+      Alert.success("Nickname Updated");
+    }
+    catch(err)
+    {
+      Alert.error(err.message, 4000);
+    }
+
+  };
 
   return (
     <>
@@ -16,13 +33,20 @@ const Dashboard = ({onSignOut}) => {
 
       <Drawer.Body>
         <h3>Welcome {profile.name}</h3>
+        <Divider/>
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2"> Nickname </h6>}
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}> Sign out </Button>
       </Drawer.Footer>
     </>
-  )
+  );
 }
 
 export default Dashboard;
